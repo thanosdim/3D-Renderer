0e84a99e12d2f17270ec39a95add6bc6484e6ac1
Thanos Dimitrakopoulos <thanosd@live.com>
2023-02-19 12:28:13 +0200

    Added basic setup and cleanup functionality

--- src/main.c
+++ src/main.c
@@ -1,4 +1,31 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+
+// Functions //
+bool setup(void);
+void cleanup(void);
+
 int main(void)
 {
+  setup();
+
+  cleanup();
+
   return 0;
+}
+
+bool setup(void)
+{
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+    fprintf(stderr, "Error: Unable to initialize SDL.\n");
+    return false;
+  }
+
+  return true;
+}
+
+void cleanup(void)
+{
+  SDL_Quit();
 }
